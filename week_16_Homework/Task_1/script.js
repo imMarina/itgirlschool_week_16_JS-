d1c35e1167d6a5a5fc55392fc10771b2b648284d
imMarina <m.shebukhova@yandex.com>
2023-09-03 17:13:16 +0200

    Registration form - task 1

---
+++ week_16_Homework/Task_1/script.js
@@ -0,0 +1,16 @@
+let collection = document.forms;
+console.log(collection);
+
+let name = collection.formOne.name
+console.log(name);
+
+let email = collection.formOne.email
+console.log(email);
+
+
+
+var password =
+document.forms.RegForm.Password.value;
+var address =
+document.forms.RegForm.Address.value;
+
