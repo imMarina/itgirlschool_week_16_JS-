ea5918c86ad4139fc3cad3539ba8b7118cce91bd
imMarina <m.shebukhova@yandex.com>
2023-09-03 21:49:06 +0200

    Is in a process

--- week_16_Homework/Task_1/script.js
+++ week_16_Homework/Task_1/script.js
@@ -1,16 +1,79 @@
+// FORMS
+
 let collection = document.forms;
-console.log(collection);
+// console.log(collection);
+let username = collection.formOne.name;
+// console.log(username);
+let email = collection.formOne.email;
+// console.log(email);
+let radioBtn = collection.formOne.gender;
+// console.log(radioBtn);
+let selectOption = collection.formOne.professions;
+// console.log(selectOption);
+let password = collection.formTwo.password;
+// console.log(password);
+let repeatPassword = collection.formTwo.repeatPassword;
+// console.log(repeatPassword);
+let checkbox = collection.formThree.agreement;
+// console.log(checkbox);
+let submit = collection.formThree.submit;
+// console.log(submit);
 
-let name = collection.formOne.name
-console.log(name);
 
-let email = collection.formOne.email
-console.log(email);
+// DOM
+const form = document.querySelector("form");
+const emailElem = form.querySelector("#email");
+const passwordElem = form.querySelector("#password");
+const repeatPasswordElem = form.querySelector("#repeatPassword");
+const submitBtn = form.querySelector(".btn");
 
 
+// DOM - errors
+const textErrorName = form.querySelector("#text-error_1");
+const textErrorEmail = form.querySelector("#text-error_2");
+const textErrorGender = form.querySelector("#text-error_3");
+const textErrorSelect = form.querySelector("#text-error_4");
+const textErrorPassword = form.querySelector("#text-error_5");
+const textErrorRepeatPassword = form.querySelector("#text-error_6");
+const textErrorCheckbox = form.querySelector("#text-error_7");
 
-var password =
-document.forms.RegForm.Password.value;
-var address =
-document.forms.RegForm.Address.value;
+
+collection.formThree.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    textErrorName.textContent = "";
+    textErrorEmail.textContent = "";
+    // textErrorPassword.textContent = "";
+    // textErrorRepeatPassword.textContent = "";
+	
+    if (username.value == "") {
+        textErrorName.textContent = "Please enter your name properly";
+    }
+    if (email.value == "") {
+        textErrorEmail.textContent = "Please enter your email properly";
+    }
+    if (password.value = "") {
+        textErrorPassword.textContent = "Please enter your password";
+    }
+    if (repeatPassword.value = "") {
+        textErrorRepeatPassword.textContent = "Please repeat your password";
+    }
+
+	if (radioBtn.checked === true) {
+		console.log("Button selected");
+	} else {
+		textErrorGender.innerHTML = "Button not selected";
+		console.log("Button not selected");
+	}
+
+    if (checkbox.checked === true) {
+		console.log("Button selected");
+	} else {
+		textErrorCheckbox.innerHTML = "Button not selected";
+		console.log("Button not selected");
+	}
+
+    collection.reset();
+  });
+
 
